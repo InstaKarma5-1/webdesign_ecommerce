6731f6b370cfd5bc59189221a1faa2738dd941ba
InstaKarma4 <lawwenjun@gmail.com>
2020-08-26 15:14:44 +0800

    Index html 2.0
    Change logs:
    - Modified index.html, .php, .css and added index.js
    - Added Slideshow, Best Sellers and Upcoming games
    - Changed search box animation
    - Slideshow:
        - Changes every 7 secs
        - the bar at the bottom acts as a selector
        - supports hover anims :star:
    - Best Sellers and Upcoming games
        - Both use the same style
        - Added their respective images (larger than the shopping-cart variant) into images/index with a x_large suffix.

---
+++ js/index.js
@@ -0,0 +1,42 @@
+var slideIndex = -1;
+var slides = document.getElementsByClassName("images");
+var bar = document.getElementsByClassName("bar");
+autoSlides();
+
+// Bar controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+//When user clicks on bar
+function showSlides(n) {
+    console.log("Length of slides is " + slides.length);
+    console.log("Length of bars is " + bar.length);
+    var i;
+    //Make slides invisible
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    //Make bars inactive
+    for (i = 0; i < bar.length; i++) {
+        bar[i].className = bar[i].className.replace(" active", "");
+    }
+
+    //Show picture and set active bar
+    slides.item(n).style.display = "block";
+    bar.item(n).className += " active";
+} 
+
+//Auto slideshow controls
+function autoSlides(){
+    //Increment slideIndex & Wrap around when slideIndex exceeds length
+    slideIndex++;
+    if(slideIndex >= slides.length){
+        slideIndex = 0;
+    }
+    //Show picture and set active bar
+    showSlides(slideIndex);
+    //Change image every 7 seconds
+    setTimeout(autoSlides,7000); 
+}
+
