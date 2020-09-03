c4fbb2bace16455a46deabf9da676e764bb50ae9
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-09-03 19:13:19 +0800

    Added admin account

--- database.php
+++ database.php
@@ -11,12 +11,12 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
 
 // Create database
 $sql = "CREATE DATABASE bayconegg";
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+  echo "Database created successfully<br>";
 } else {
   echo "Error creating database: " . $conn->error;
 }
@@ -30,20 +30,32 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
+// Create user table
 $sql = "CREATE TABLE user (
     userId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(50) NOT NULL,
     passwd VARCHAR(50) NOT NULL,
-    phone VARCHAR(12) NOT NULL
+    phone VARCHAR(12) NOT NULL,
+    isAdmin BOOLEAN NOT NULL DEFAULT 0
     )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table 'user' created successfully";
+    echo "Table 'user' created successfully<br>";
 } else {
     echo "Error creating table: " . $conn->error;
 }
 
+//Insert admin into user table
+$sql = "INSERT INTO user (email, passwd, phone, isAdmin)
+        VALUES ('admin@bayconeggs.com', 'admin', '019-6969691', true)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Admin account created successfully<br>";
+} else {
+  echo "Error when creating admin account: " . $conn->error;
+}
+
+// Create security_question table
 $sql = "CREATE TABLE security_question (
   userId INT(6) UNSIGNED AUTO_INCREMENT,
   question VARCHAR(255) NOT NULL,
@@ -55,9 +67,24 @@ $sql = "CREATE TABLE security_question (
   )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table 'security_question' created successfully";
+  echo "Table 'security_question' created successfully<br>";
 } else {
   echo "Error creating table: " . $conn->error;
+}
+
+// Create games table
+$sql = "CREATE TABLE games (
+gameId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+gameName VARCHAR(255) NOT NULL,
+gameDesc TEXT NOT NULL,
+gamePic VARCHAR(255) NOT NULL,
+gamePrice DOUBLE NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+echo "Table 'games' created successfully<br>";
+} else {
+echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
