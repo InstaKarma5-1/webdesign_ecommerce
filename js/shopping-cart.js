5e4159f2889c3825eb471203f011c49290f0803f
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-09-08 19:48:46 +0800

    Shopping Cart JS

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,25 +1,45 @@
-let decreaseButtons = document.getElementsByClassName("shopping-cart-decrease-button");
-let increaseButtons = document.getElementsByClassName("shopping-cart-increase-button");
-let priceArray = document.getElementsByClassName("shopping-cart-item-price");
-let quantityArray = document.getElementsByClassName("shopping-cart-quantity-value");
-let totalPrice = parseFloat(document.getElementById("shopping-cart-total-value").innerHTML);
+decreaseButtons = document.getElementsByClassName("shopping-cart-decrease-button");
+increaseButtons = document.getElementsByClassName("shopping-cart-increase-button");
+deleteButtons = document.getElementsByClassName("shopping-cart-delete-button");
+shoppingCartItems = document.getElementsByClassName("shopping-cart-item");
+horizontalRules = document.getElementsByClassName("shopping-cart-horizontal-rule");
+priceArray = document.getElementsByClassName("shopping-cart-item-price");
+quantityArray = document.getElementsByClassName("shopping-cart-quantity-value");
+totalPrice = parseFloat(document.getElementById("shopping-cart-total-value").innerHTML);
 
-// Assign event listener to all buttons for each individual cart item
-for (let i = 0; i < decreaseButtons.length ; i++) {
-    decreaseButtons[i].addEventListener("click", function() {decrement(i)});
+stackoverflow = "https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function";
+alert(stackoverflow);
+
+updateElements();
+updateTotal();
+
+function updateElements() {
+    if(shoppingCartItems.length != 0 ){
+        // Remove event listener to all buttons for each individual cart item
+        for (let i = 0; i < decreaseButtons.length ; i++) {
+            decreaseButtons[i].removeEventListener("click", decrement); //parameter is i for each function
+            increaseButtons[i].removeEventListener("click", increment);
+            deleteButtons[i].removeEventListener("click", remove);
+        }
+    }  
+
+    if(shoppingCartItems.length != 0 ){
+        // Assign event listener to all buttons for each individual cart item
+        for (let i = 0; i < decreaseButtons.length ; i++) {
+            decreaseButtons[i].addEventListener("click", decrement);
+            increaseButtons[i].addEventListener("click", increment);
+            deleteButtons[i].addEventListener("click", remove);
+        }
+    }   
 }
-
-for (let i = 0; i < increaseButtons.length ; i++) {
-    increaseButtons[i].addEventListener("click", function() {increment(i)});
-}
-
 
 function decrement(i) {
+    event.currentTarget.my
     let quantity = decreaseButtons[i].nextElementSibling.innerHTML;
     if (quantity > 1) {
         quantity--;
         decreaseButtons[i].nextElementSibling.innerHTML = quantity;
-        updateTotal(i);
+        updateTotal();
     }
 }
 
@@ -28,13 +48,26 @@ function increment(i) {
     if (quantity < 69) {
         quantity++;
         increaseButtons[i].previousElementSibling.innerHTML = quantity;
-        updateTotal(i);
+        updateTotal();
     }
 }
 
-function updateTotal(i) {
-    for (let i = 0; i < priceArray.length ; i++) {
-        totalPrice = parseFloat(priceArray[i].innerHTML) * parseInt(quantityArray[i].innerHTML);   //this code-o is subarashi
+function remove(i) {
+    let confirmRemove = confirm("Are you sure you want to remove this item from the cart?");
+    console.log("I is " + i);
+    if(confirmRemove){
+        shoppingCartItems[i].remove();
+        horizontalRules[i].remove();
+        updateElements();
     }
-    console.log("Total price is RM" + totalPrice);
+    updateTotal();
 }
+
+function updateTotal() {
+    totalPrice = 0;
+    for (let i = 0; i < priceArray.length ; i++) {
+        totalPrice += parseFloat(priceArray[i].innerHTML) * parseInt(quantityArray[i].innerHTML);
+    }
+    document.getElementById("shopping-cart-total-value").innerHTML = totalPrice.toFixed(2);
+}
+
