45a3952d0844a1bdd4e896dc6fa431c9f2c2270a
marcuswkl <56200199+marcuswkl@users.noreply.github.com>
2020-09-16 16:55:20 +0800

    Embed Google Maps in contact.php

--- contact.php
+++ contact.php
@@ -31,6 +31,9 @@
                     <td>Axiata Tower, Level 20, 9, Jalan Stesen Sentral 5, Kuala Lumpur Sentral, 50470 Kuala Lumpur, Federal Territory of Kuala Lumpur</td>
                 </tr>
             </table>
+            <br>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.8517724721037!2d101.6824928152521!3d3.133839654119286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc499892220cc9%3A0x9a46bc26ac73e47!2sAxiata%20Tower!5e0!3m2!1sen!2smy!4v1600246376068!5m2!1sen!2smy" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
         </div>
         <br>
         <?php include "footer.php"; ?>
